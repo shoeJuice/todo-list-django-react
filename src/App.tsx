d7dbea772652340054cc2f5d3f8edd187c3a2f9c
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-06-25 18:08:20 -0400

    First Major Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import {Text, Flex, VStack, StackDivider} from '@chakra-ui/react'
+
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem'
+import TodoListContainer from './components/TodoListContainer';
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Text
+        textAlign='center'
+        color='white'
+        marginBottom={5}
+        fontSize='2xl'
+        fontWeight='medium'
+      >
+        Todo List!
+      </Text>
+      <TodoListContainer>
+        <VStack
+          alignItems='space-between'
+          divider={<StackDivider borderColor='blackAlpha.100' />}
         >
-          Learn React
-        </a>
-      </header>
+          
+        </VStack>
+      </TodoListContainer>
     </div>
   );
 }
